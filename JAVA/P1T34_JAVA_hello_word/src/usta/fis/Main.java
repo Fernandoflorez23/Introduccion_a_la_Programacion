c851e148253b8f39b60d41d197eed6cd7db53d86
Fernando Florez Caballero <jhoan.florez@usantoto.edu.co>
2021-03-16 17:20:32 -0500

    Last ejercices dfd

---
+++ JAVA/P1T34_JAVA_hello_word/src/usta/fis/Main.java
@@ -0,0 +1,18 @@
+package usta.fis;
+
+//AUTHOR: Jhoan Fernando Florez Caballero
+//DATE: March 16 2021
+//DESCRIPTION: This is my first program in java
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("----------------------------------");
+        System.out.println("Softtheworld version 1.0 ");
+        System.out.println("AUTHOR: Jhoan Fernando Florez Caballero");
+        System.out.println("DESCRIPTION: This is my first program in java and show the word hello");
+        System.out.println("----------------------------------");
+        System.out.println("Hello world");
+    }
+}
